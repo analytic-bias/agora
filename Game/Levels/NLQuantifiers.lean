96862af0c10e5523ad8a367b200509b8857b45e6
Hypatia du Bois-Marie <knight.of.lambda.calculus@gmail.com>
2023-10-22 06:48:45 -0400

    a

--- Game/Levels/NLQuantifiers.lean
+++ Game/Levels/NLQuantifiers.lean
@@ -8,7 +8,7 @@ Introduction
 "
 # Non-associative Lambek Calculus in Quantifier' Grammar
 
-The specification of System **NL** is partially adapted from [Kokke, P. (n.d.). Formalising type-logical grammars in Agda.](https://www.lirmm.fr/tytles/Articles/Kokke.pdf), whose code was originally written in ![Agda](https://gist.github.com/wenkokke/cc12b92a8a60696b712c).
+The specification of System **NL** is partially adapted from [Kokke, P. (n.d.). Formalising type-logical grammars in Agda.](https://www.lirmm.fr/tytles/Articles/Kokke.pdf), whose code was originally written in [Agda](https://gist.github.com/wenkokke/cc12b92a8a60696b712c).
 
 The exercise in this World is also from loc. cit.
 "
