a618fbf2600927bbbe3ef6edff11f1c44894e9b9
Hypatia du Bois-Marie #RemoveCCP #HackCCP #中共下台 #FormalSciencesForHumanRight #形式科學爲人權 <knight.of.lambda.calculus@gmail.com>
2023-10-22 03:51:06 +0000

    a

--- Game/Levels/MyNL/NL.lean
+++ Game/Levels/MyNL/NL.lean
@@ -17,15 +17,15 @@ inductive NLJudgement where
 
 infixr:0 "⊢" => NLJudgement.judge
 
-inductive NL : (NLJudgement atom) -> Type where
-| ax : NL (!a ⊢ !a)
-| residuation_rm : NL (b ⊢ a \ c) -> NL (a ⊗ b ⊢ c)
-| residuation_mr : NL (a ⊗ b ⊢ c) -> NL (b ⊢ a \ c)
-| residuation_lm : NL (a ⊢ c / b) -> NL (a ⊗ b ⊢ c)
-| residuation_ml : NL (a ⊗ b ⊢ c) -> NL (a ⊢ c / b)
-| monotonicity_times : NL (a ⊢ b) -> NL (c ⊢ d) -> NL (a ⊗ c ⊢ b ⊗ d)
-| monotonicity_slash : NL (a ⊢ b) -> NL (c ⊢ d) -> NL (a / d ⊢ b / c)
-| monotonicity_backslash : NL (a ⊢ b) -> NL (c ⊢ d) -> NL (d \ a ⊢ c \ b)
+inductive NLCalculus : (NLJudgement atom) -> Type where
+| reflexivity : NLCalculus (!a ⊢ !a)
+| residuation_rm {a b c} : NLCalculus (b ⊢ a \ c) -> NLCalculus (a ⊗ b ⊢ c)
+| residuation_mr {a b c} : NLCalculus (a ⊗ b ⊢ c) -> NLCalculus (b ⊢ a \ c)
+| residuation_lm {a b c} : NLCalculus (a ⊢ c / b) -> NLCalculus (a ⊗ b ⊢ c)
+| residuation_ml {a b c} : NLCalculus (a ⊗ b ⊢ c) -> NLCalculus (a ⊢ c / b)
+| monotonicity_times {a b c} : NLCalculus (a ⊢ b) -> NLCalculus (c ⊢ d) -> NLCalculus (a ⊗ c ⊢ b ⊗ d)
+| monotonicity_slash {a b c} : NLCalculus (a ⊢ b) -> NLCalculus (c ⊢ d) -> NLCalculus (a / d ⊢ b / c)
+| monotonicity_backslash {a b c} : NLCalculus (a ⊢ b) -> NLCalculus (c ⊢ d) -> NLCalculus (d \ a ⊢ c \ b)
 end NL
 
 namespace NLInterpretation
@@ -38,7 +38,17 @@ notation:max (priority := high) "~" a => (a -> Value)
 def interpret : (NLType atom) -> Type
   | !a => interpret_atom a
   | a ⊗ b => (interpret a) × (interpret b)
-  | a / b => ~((interpret a) × ~(interpret b))
-  | a \ b => ~(~(interpret a) × (interpret b))
+  | a \ b => ~((interpret a) × (~(interpret b)))
+  | a / b => ~((~(interpret a)) × (interpret b))
+
+def deMorgan : (~~a) -> (~~b) -> ~~(a × b) := fun c1 c2 k =>
+  c1 (fun x => c2 (fun y => k (x, y)))
+
+def l (c : NLCalculus _ (a ⊢ b)) :
+      (~(interpret interpret_atom Value b)) -> ~(interpret interpret_atom Value a) := match c with
+  | NLCalculus.reflexivity => fun k x => k x
+  -- ⌈ r⇒⊗ f    ⌉ᴸ   x  =    λ{(y , z) → ⌈ f ⌉ᴸ (λ k → k (y , x)) z}
+  | NLCalculus.residuation_rm f => fun x => fun | (y, z) => (l f) (fun k => k (y, x)) z
+
 
 end NLInterpretation
